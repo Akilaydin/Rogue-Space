e7414e7ec51ef722dd95b80d0aa3d357f14c97ab
Артём Овчинников <akilaydin2@gmail.com>
2020-12-24 00:58:53 +0300

    Added settings panel.
    Added settings panel. In there added half-working sound setters.

---
+++ Assets/Scripts/UI scripts/SoundsSettings.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class SoundsSettings : MonoBehaviour
+{
+    public AudioMixer musicMixer;
+    public AudioMixer effectMixer;
+
+    public void setVolumeToMusic(float volume){
+        musicMixer.SetFloat("musicVolume",volume);
+    }
+
+    public void setVolumeToEffects(float volume){
+        effectMixer.SetFloat("effectVolume",volume);
+    }
+}
