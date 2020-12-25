da4824362020e295201a8e67d40e959e1702f420
Артём Овчинников <akilaydin2@gmail.com>
2020-12-25 16:39:01 +0300

    Begun to add an integration with Google Play Services.
    Begun to add an integration with Google Play Services.

---
+++ Assets/Scripts/GooglePlayAuthentification.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine.SocialPlatforms;
+
+public class GooglePlayAuthentification : MonoBehaviour
+{
+
+    public static PlayGamesPlatform gpPlatformInstance;
+   
+    void Awake(){
+        if (gpPlatformInstance == null){
+            PlayGamesClientConfiguration configuration = new PlayGamesClientConfiguration.Builder().EnableSavedGames().Build();
+            PlayGamesPlatform.InitializeInstance(configuration);
+            PlayGamesPlatform.DebugLogEnabled = true;
+
+            gpPlatformInstance = PlayGamesPlatform.Activate();
+
+        }
+
+        Social.Active.localUser.Authenticate(success => 
+        {
+            if (success){
+                Debug.Log("Logged in successfully");
+
+            } else {
+                Debug.Log("Failed logging in");
+            }
+        });
+    }
+
+
+}
