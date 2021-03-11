15bc73a2255f43d1ba097c98d92bb203a9ad3a9a
Akilaydin <akilaydin2@gmail.com>
2021-03-11 15:49:51 +0300

    Working on the upgrade system.
    Working on the upgrade system.

--- Assets/Scripts/GameScripts/Database.cs
+++ Assets/Scripts/GameScripts/Database.cs
@@ -15,9 +15,23 @@ public class DataSave
 public class UpgradesData //Показывает не сами параметры, а степень их улучшения.
 {
     public float damageUpgrade;
+    public int damageUpgradeLevel;
+    public int damageUpgradeCost; 
+
+
     public float hpUpgrade;
+    public int hpUpgradeLevel;
+    public int hpUpgradeCost; 
+
+
     public float fireRateUpgrade;
+    public int fireRateUpgradeLevel;
+    public int fireRateUpgradeCost; 
+
+
     public int shipLevel; //Пока не используется
+    public int shipLevelUpgradeLevel;
+    
 
 }
 public class Database : MonoBehaviour
@@ -35,45 +49,53 @@ public class Database : MonoBehaviour
         }
 
         DontDestroyOnLoad(this.gameObject);
+        Debug.Log(Application.persistentDataPath);
     }
 
     #region UpgradeSaves
-    public void SaveDamageUpgrade(float damageUpgrade)
+    public void SaveDamageUpgrade(float damageUpgrade, float damageUpgradeCostIncremention)
     {
-        BinaryFormatter bf = new BinaryFormatter();
-        FileStream file = File.Create(Application.persistentDataPath + "/Upgrades.dat");
         UpgradesData upgrades = new UpgradesData();
-        upgrades.damageUpgrade += damageUpgrade;
+        upgrades.damageUpgrade = LoadDamageUpgrade() + damageUpgrade;
+        upgrades.damageUpgradeLevel = LoadDamageUpgradeLevel() + 1;
+        upgrades.damageUpgradeCost = (int) (LoadDamageUpgradeCost() * damageUpgradeCostIncremention);
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/UpgradesDamage.dat");
         bf.Serialize(file, upgrades);
         file.Close();
     }
 
-    public void SaveHpUpgrade(float hpUpgrade)
+    public void SaveHpUpgrade(float hpUpgrade, float hpUpgradeCostIncremention)
     {
-        BinaryFormatter bf = new BinaryFormatter();
-        FileStream file = File.Create(Application.persistentDataPath + "/Upgrades.dat");
         UpgradesData upgrades = new UpgradesData();
-        upgrades.hpUpgrade += hpUpgrade;
+        upgrades.hpUpgrade = LoadHpUpgrade() + hpUpgrade;
+        upgrades.hpUpgradeLevel = LoadHpUpgradeLevel() + 1;
+        upgrades.hpUpgradeCost = (int) (LoadFireRateUpgradeCost() * hpUpgradeCostIncremention);
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/UpgradesHp.dat");
         bf.Serialize(file, upgrades);
         file.Close();
     }
 
-    public void SaveFireRateUpgrade(float fireRate)
+    public void SaveFireRateUpgrade(float fireRateUpgrade, float fireRateUpgradeCostIncremention)
     {
-        BinaryFormatter bf = new BinaryFormatter();
-        FileStream file = File.Create(Application.persistentDataPath + "/Upgrades.dat");
         UpgradesData upgrades = new UpgradesData();
-        upgrades.fireRateUpgrade += fireRate;
+        upgrades.fireRateUpgrade += LoadFireRateUpgrade() + fireRateUpgrade;
+        upgrades.fireRateUpgradeLevel = LoadFireRateUpgradeLevel() + 1;
+        upgrades.fireRateUpgradeCost = (int) (LoadFireRateUpgradeCost() * fireRateUpgradeCostIncremention);
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/UpgradesFireRate.dat");
         bf.Serialize(file, upgrades);
         file.Close();
     }
 
     public void SaveShipLevelUpgrade(int shipLevel)
     {
-        BinaryFormatter bf = new BinaryFormatter();
-        FileStream file = File.Create(Application.persistentDataPath + "/Upgrades.dat");
         UpgradesData upgrades = new UpgradesData();
-        upgrades.shipLevel += shipLevel;
+        upgrades.shipLevel = LoadShipLevelUpgrade() + shipLevel;
+        upgrades.shipLevelUpgradeLevel = LoadShipLevelUpgradeLevel() + 1;
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/UpgradesShipLevel.dat");
         bf.Serialize(file, upgrades);
         file.Close();
     }
@@ -85,10 +107,10 @@ public class Database : MonoBehaviour
     #region UpgradeLoads
     public float LoadDamageUpgrade()
     {
-        if (File.Exists(Application.persistentDataPath + "/Upgrades.dat"))
+        if (File.Exists(Application.persistentDataPath + "/UpgradesDamage.dat"))
         {
             BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Open(Application.persistentDataPath + "/Upgrades.dat", FileMode.Open);
+            FileStream file = File.Open(Application.persistentDataPath + "/UpgradesDamage.dat", FileMode.Open);
             UpgradesData upgrades = (UpgradesData)bf.Deserialize(file);
             file.Close();
             return upgrades.damageUpgrade;
@@ -98,12 +120,42 @@ public class Database : MonoBehaviour
         return 0;
     }
 
-    public float LoadHpUpgrade()
+    public int LoadDamageUpgradeLevel()
     {
-        if (File.Exists(Application.persistentDataPath + "/Upgrades.dat"))
+        if (File.Exists(Application.persistentDataPath + "/UpgradesDamage.dat"))
         {
             BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Open(Application.persistentDataPath + "/Upgrades.dat", FileMode.Open);
+            FileStream file = File.Open(Application.persistentDataPath + "/UpgradesDamage.dat", FileMode.Open);
+            UpgradesData upgrades = (UpgradesData)bf.Deserialize(file);
+            file.Close();
+            return upgrades.damageUpgradeLevel;
+        }
+        else
+            Debug.LogError("There is no save data for damage");
+        return 0;
+    }
+
+    public int LoadDamageUpgradeCost()
+    {
+        if (File.Exists(Application.persistentDataPath + "/UpgradesDamage.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/UpgradesDamage.dat", FileMode.Open);
+            UpgradesData upgrades = (UpgradesData)bf.Deserialize(file);
+            file.Close();
+            return upgrades.damageUpgradeCost;
+        }
+        else
+            Debug.LogError("There is no save data for damage");
+        return 0;
+    }
+
+    public float LoadHpUpgrade()
+    {
+        if (File.Exists(Application.persistentDataPath + "/UpgradesHp.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/UpgradesHp.dat", FileMode.Open);
             UpgradesData upgrades = (UpgradesData)bf.Deserialize(file);
             file.Close();
             return upgrades.hpUpgrade;
@@ -113,12 +165,42 @@ public class Database : MonoBehaviour
         return 0;
     }
 
-    public float LoadFireRateUpgrade()
+    public int LoadHpUpgradeLevel()
     {
-        if (File.Exists(Application.persistentDataPath + "/Upgrades.dat"))
+        if (File.Exists(Application.persistentDataPath + "/UpgradesHp.dat"))
         {
             BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Open(Application.persistentDataPath + "/Upgrades.dat", FileMode.Open);
+            FileStream file = File.Open(Application.persistentDataPath + "/UpgradesHp.dat", FileMode.Open);
+            UpgradesData upgrades = (UpgradesData)bf.Deserialize(file);
+            file.Close();
+            return upgrades.hpUpgradeLevel;
+        }
+        else
+            Debug.LogError("There is no save data for hp");
+        return 0;
+    }
+
+    public int LoadHpUpgradeCost()
+    {
+        if (File.Exists(Application.persistentDataPath + "/UpgradesDamage.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/UpgradesDamage.dat", FileMode.Open);
+            UpgradesData upgrades = (UpgradesData)bf.Deserialize(file);
+            file.Close();
+            return upgrades.hpUpgradeCost;
+        }
+        else
+            Debug.LogError("There is no save data for hp");
+        return 0;
+    }
+
+    public float LoadFireRateUpgrade()
+    {
+        if (File.Exists(Application.persistentDataPath + "/UpgradesFireRate.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/UpgradesFireRate.dat", FileMode.Open);
             UpgradesData upgrades = (UpgradesData)bf.Deserialize(file);
             file.Close();
             return upgrades.fireRateUpgrade;
@@ -128,12 +210,42 @@ public class Database : MonoBehaviour
         return 0;
     }
 
-    public float LoadShipLevelUpgrade()
+    public int LoadFireRateUpgradeLevel()
     {
-        if (File.Exists(Application.persistentDataPath + "/Upgrades.dat"))
+        if (File.Exists(Application.persistentDataPath + "/UpgradesFireRate.dat"))
         {
             BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Open(Application.persistentDataPath + "/Upgrades.dat", FileMode.Open);
+            FileStream file = File.Open(Application.persistentDataPath + "/UpgradesFireRate.dat", FileMode.Open);
+            UpgradesData upgrades = (UpgradesData)bf.Deserialize(file);
+            file.Close();
+            return upgrades.fireRateUpgradeLevel;
+        }
+        else
+            Debug.LogError("There is no save data for fire rate");
+        return 0;
+    }
+
+    public int LoadFireRateUpgradeCost()
+    {
+        if (File.Exists(Application.persistentDataPath + "/UpgradesDamage.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/UpgradesDamage.dat", FileMode.Open);
+            UpgradesData upgrades = (UpgradesData)bf.Deserialize(file);
+            file.Close();
+            return upgrades.fireRateUpgradeCost;
+        }
+        else
+            Debug.LogError("There is no save data for fire rate");
+        return 0;
+    }
+
+    public int LoadShipLevelUpgrade()
+    {
+        if (File.Exists(Application.persistentDataPath + "/UpgradesShipLevel.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/UpgradesShipLevel.dat", FileMode.Open);
             UpgradesData upgrades = (UpgradesData)bf.Deserialize(file);
             file.Close();
             return upgrades.shipLevel;
@@ -143,6 +255,35 @@ public class Database : MonoBehaviour
         return 0;
     }
 
+    public int LoadShipLevelUpgradeLevel()
+    {
+        if (File.Exists(Application.persistentDataPath + "/UpgradesShipLevel.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/UpgradesShipLevel.dat", FileMode.Open);
+            UpgradesData upgrades = (UpgradesData)bf.Deserialize(file);
+            file.Close();
+            return upgrades.shipLevelUpgradeLevel;
+        }
+        else
+            Debug.LogError("There is no save data for ship level");
+        return 0;
+    }
+
+    public int LoadShipLevelUpgradeCost()
+    {
+        if (File.Exists(Application.persistentDataPath + "/UpgradesDamage.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/UpgradesDamage.dat", FileMode.Open);
+            UpgradesData upgrades = (UpgradesData)bf.Deserialize(file);
+            file.Close();
+            return upgrades.fireRateUpgradeCost;
+        }
+        else
+            Debug.LogError("There is no save data for ship level");
+        return 0;
+    }
     #endregion
 
 
@@ -178,7 +319,7 @@ public class Database : MonoBehaviour
             file.Close();
             return data.savedScore;
         }
-        else
+        else 
             Debug.LogError("There is no save data!");
         return 0;
 
--- Assets/Scripts/UI scripts/UpgradesController.cs
+++ Assets/Scripts/UI scripts/UpgradesController.cs
@@ -1,26 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class UpgradesController : MonoBehaviour
 {
+
+
     [SerializeField]
     private int damageUpgradeCost = 100;
     [SerializeField]
+    private float damageUpgradeCostIncremention = 1.6f;
+    [SerializeField]
+    private float damageUpgradeDegree = 0.1f;
+
+
+
+    [SerializeField]
     private int hpUpgradeCost = 80;
+    [SerializeField]
+    private float hpUpgradeCostIncremention = 1.4f;
+    [SerializeField]
+    private float hpUpgradeDegree = 0.1f;
+
+
+
     [SerializeField]
     private int fireRateUpgradeCost = 150;
     [SerializeField]
-    private float damageUpgradeDegree = 0.1f;
+    private float fireRateUpgradeDegree = 0.01f;
     [SerializeField]
-    private float hpUpgradeDegree = 0.1f;
+    private float fireRateUpgradeCostIncremention = 2.5f;
+
+
     [SerializeField]
-    private float fireRateUpgradeDegree = 0.1f;
+    private Text hpCostText,hpLevelText;
+    [SerializeField]
+    private Text damageCostText,damageLevelText;
+    [SerializeField]
+    private Text fireRateCostText,fireRateLevelText;
+
+    private string upgradeLevelTemplate = "Уровень улучшения: ";
+    private string upgradeCostTemplate = "Стоимость улучшения: ";
 
     private int totalScore;
 
     private void Start() {
         totalScore = Database.instance.LoadGameScore();
+
+        UpgradeLevelOfDamage();
+        UpgradeLevelOfFireRate();
+        UpgradeLevelOfHP();
     }
     public void UpgradeDamage()
     {
@@ -28,9 +57,9 @@ public class UpgradesController : MonoBehaviour
         {
             totalScore -= damageUpgradeCost;
             Database.instance.SaveGameScore(false,totalScore);
-            Database.instance.SaveDamageUpgrade(damageUpgradeDegree);
+            Database.instance.SaveDamageUpgrade(damageUpgradeDegree,damageUpgradeCostIncremention * damageUpgradeCost);
             MenuController.instance.RefreshScoreInMenu();
-            Debug.Log("Damage upgraded succesfully");
+            UpgradeLevelOfDamage();
         } 
         else {
             Debug.Log("Dont have enough money");
@@ -43,9 +72,9 @@ public class UpgradesController : MonoBehaviour
         {
             totalScore -= hpUpgradeCost;
             Database.instance.SaveGameScore(false,totalScore);
-            Database.instance.SaveHpUpgrade(hpUpgradeDegree);
+            Database.instance.SaveHpUpgrade(hpUpgradeDegree,hpUpgradeCostIncremention * hpUpgradeCost);
             MenuController.instance.RefreshScoreInMenu();
-            Debug.Log("Hp upgraded succesfully");
+            UpgradeLevelOfHP();
         } 
         else {
             Debug.Log("Dont have enough money");
@@ -58,13 +87,32 @@ public class UpgradesController : MonoBehaviour
         {
             totalScore -= fireRateUpgradeCost;
             Database.instance.SaveGameScore(false,totalScore);
-            Database.instance.SaveFireRateUpgrade(fireRateUpgradeDegree);
+            Database.instance.SaveFireRateUpgrade(fireRateUpgradeDegree,fireRateUpgradeCostIncremention * fireRateUpgradeCost);
             MenuController.instance.RefreshScoreInMenu();
-            Debug.Log("Fire rate upgraded succesfully");
+            UpgradeLevelOfFireRate();
         } 
         else {
             Debug.Log("Dont have enough money");
         }
     }
 
+
+
+    private void UpgradeLevelOfHP()
+    {
+        hpLevelText.text = upgradeLevelTemplate + Database.instance.LoadHpUpgradeLevel().ToString();
+        hpCostText.text = upgradeCostTemplate + Database.instance.LoadHpUpgradeCost().ToString();
+    }
+
+    private void UpgradeLevelOfDamage()
+    {
+        damageLevelText.text = upgradeLevelTemplate + Database.instance.LoadDamageUpgradeLevel().ToString();
+        damageCostText.text = upgradeCostTemplate + Database.instance.LoadDamageUpgradeCost().ToString();
+    }
+
+    private void UpgradeLevelOfFireRate()
+    {
+        fireRateLevelText.text = upgradeLevelTemplate + Database.instance.LoadFireRateUpgradeLevel().ToString();
+        fireRateCostText.text = upgradeCostTemplate + Database.instance.LoadFireRateUpgradeCost().ToString();
+    }
 }
